f16a3872380f406466ef9cdefa1807441ba289d1
Mohamed_Yasser <mohamed.yasser99999@gmail.com>
2023-06-13 23:43:55 +0300

    Adding new Features

--- ecommerce/src/main/java/com/example/practise/spring/service/implementations/CategoryServiceImpl.java
+++ ecommerce/src/main/java/com/example/practise/spring/service/implementations/CategoryServiceImpl.java
@@ -1,17 +1,52 @@
 package com.example.practise.spring.service.implementations;
 import com.example.practise.spring.entity.Category;
 import com.example.practise.spring.repository.CategoryRepository;
+import com.example.practise.spring.service.interfaces.CategoryService;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 @Service
-public class CategoryService implements com.example.practise.spring.service.interfaces.CategoryService {
-    @Autowired
+public class CategoryServiceImpl implements com.example.practise.spring.service.interfaces.CategoryService {
+    @Mock
     private CategoryRepository categoryRepository;
-    public List<Category> get() {
-        return categoryRepository.findByActivatedTrue();
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @Test
+    void get() {
+        // Arrange
+        List<Category> expectedCategories = new ArrayList<>();
+        Category category1 = new Category();
+        category1.setId(1L);
+        category1.setName("Test Category 1");
+        Category category2 = new Category();
+        category2.setId(2L);
+        category2.setName("Test Category 2");
+        expectedCategories.add(category1);
+        expectedCategories.add(category2);
+
+        // Mock the categoryRepository
+        Mockito.when(categoryRepository.findByActivatedTrue()).thenReturn(expectedCategories);
+
+        // Act
+        List<Category> actualCategories = categoryService.get();
+
+        // Assert
+        assertEquals(expectedCategories.size(), actualCategories.size());
+        assertEquals(expectedCategories.get(0).getId(), actualCategories.get(0).getId());
+        assertEquals(expectedCategories.get(0).getName(), actualCategories.get(0).getName());
+        assertEquals(expectedCategories.get(1).getId(), actualCategories.get(1).getId());
+        assertEquals(expectedCategories.get(1).getName(), actualCategories.get(1).getName());
     }
 
     public Category add(Category category) {
